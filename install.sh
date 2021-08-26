a9e90410a696b8c61a228eb408d0938ce7f1cdb7
casjay <casjay@yahoo.com>
2021-08-26 10:29:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME="hub"
+APPNAME="gitea"
 VERSION="202107311147-git"
 USER="${SUDO_USER:-${USER}}"
 HOME="${USER_HOME:-${HOME}}"
 SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #set opts
+exit 1
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version       : 202107311147-git
@@ -16,8 +17,8 @@ SRC_DIR="${BASH_SOURCE%/*}"
 # @ReadME        : dockermgr --help
 # @Copyright     : Copyright: (c) 2021 casjay, casjay
 # @Created       : Saturday, Jul 31, 2021 11:47 EDT
-# @File          : hub
-# @Description   : hub docker container installer
+# @File          : gitea
+# @Description   : gitea docker container installer
 # @TODO          :
 # @Other         :
 # @Resource      :
@@ -40,17 +41,19 @@ fi
 # user system devenv dfmgr dockermgr fontmgr iconmgr pkmgr systemmgr thememgr wallpapermgr
 dockermgr_install
 __options "$@"
+__sudo() { if sudo -n true; then eval sudo "$*"; else eval "$*"; fi; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Begin installer
 APPNAME="hub"
 DOCKER_HUB_URL=""
+HUB_SERVER_PORT="${HUB_SERVER_PORT:-443}"
+HUB_SERVER_HOST="${HUB_SERVER_HOST:-$(hostname -f 2>/dev/null)}"
+REPO="${DOCKERMGRREPO:-https://github.com/dockermgr}/$APPNAME"
+REPO_BRANCH="${GIT_REPO_BRANCH:-main}"
+GITEA_SERVER_TIMEZONE="${TZ:-${TIMEZONE:-America/New_York}}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPDIR="/usr/local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
-INSTDIR="/usr/local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
-DATADIR="/srv/docker/$APPNAME"
-REPORAW="$REPO/raw/$GIT_DEFAULT_BRANCH"
+REPORAW="$REPO/raw/$REPO_BRANCH"
 APPVERSION="$(__appversion "$REPORAW/version.txt")"
-TIMEZONE="${TZ:-$TIMEZONE}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 printf_blue "Initializing the hub installer"
 # install portainer
@@ -60,4 +63,5 @@ sudo dockermgr install registry
 # install registry frontend
 sudo dockermgr install registry-web
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End script
 exit $?
