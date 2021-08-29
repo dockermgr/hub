31366d0ee49d5113eff9c844eb40c96edc2b15f9
casjay <casjay@yahoo.com>
2021-08-28 20:51:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,66 +1,141 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME="gitea"
-VERSION="202107311147-git"
+APPNAME="hub"
 USER="${SUDO_USER:-${USER}}"
 HOME="${USER_HOME:-${HOME}}"
 SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-#set opts
+# Set bash options
+if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version       : 202107311147-git
-# @Author        : casjay
-# @Contact       : casjay
+##@Version       : 202108282020-git
+# @Author        : Jason Hempstead
+# @Contact       : jason@casjaysdev.com
 # @License       : WTFPL
-# @ReadME        : dockermgr --help
-# @Copyright     : Copyright: (c) 2021 casjay, casjay
-# @Created       : Saturday, Jul 31, 2021 11:47 EDT
-# @File          : gitea
-# @Description   : gitea docker container installer
-# @TODO          :
-# @Other         :
-# @Resource      :
+# @ReadME        : hub --help
+# @Copyright     : Copyright: (c) 2021 Jason Hempstead, Casjays Developments
+# @Created       : Saturday, Aug 28, 2021 20:20 EDT
+# @File          : hub
+# @Description   : 
+# @TODO          : 
+# @Other         : 
+# @Resource      : 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Import functions
 CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
 SCRIPTSFUNCTDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}/functions"
-SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-testing.bash}"
-SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/$GIT_DEFAULT_BRANCH/functions}"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/main/functions}"
+connect_test() { ping -c1 1.1.1.1 &>/dev/null || curl --disable -LSs --connect-timeout 3 --retry 0 --max-time 1 1.1.1.1 2>/dev/null | grep -e "HTTP/[0123456789]" | grep -q "200" -n1 &>/dev/null; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if [ -f "$PWD/$SCRIPTSFUNCTFILE" ]; then
   . "$PWD/$SCRIPTSFUNCTFILE"
 elif [ -f "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" ]; then
   . "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE"
+elif connect_test; then
+  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/$SCRIPTSFUNCTFILE" || exit 1
+  . "/tmp/$SCRIPTSFUNCTFILE"
 else
   echo "Can not load the functions file: $SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" 1>&2
   exit 1
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# user system devenv dfmgr dockermgr fontmgr iconmgr pkmgr systemmgr thememgr wallpapermgr
-dockermgr_install
-__options "$@"
+# Call the main function
+user_installdirs
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Define extra functions
 __sudo() { if sudo -n true; then eval sudo "$*"; else eval "$*"; fi; }
+__enable_ssl() { [[ "$SERVER_SSL" = "yes" ]] && [[ "$SERVER_SSL" = "true" ]] && return 0 || return 1; }
+__ssl_certs() { [ -f "${1:-$SERVER_SSL_CRT}" ] && [ -f "${2:-SERVER_SSL_KEY}" ] && return 0 || return 1; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Begin installer
+# Make sure the scripts repo is installed
+scripts_check
+REPO_BRANCH="${GIT_REPO_BRANCH:-master}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Defaults
 APPNAME="hub"
-DOCKER_HUB_URL=""
-HUB_SERVER_PORT="${HUB_SERVER_PORT:-443}"
-HUB_SERVER_HOST="${HUB_SERVER_HOST:-$(hostname -f 2>/dev/null)}"
-REPO="${DOCKERMGRREPO:-https://github.com/dockermgr}/$APPNAME"
-REPO_BRANCH="${GIT_REPO_BRANCH:-main}"
-GITEA_SERVER_TIMEZONE="${TZ:-${TIMEZONE:-America/New_York}}"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPDIR="$HOME/.local/share/docker/hub"
+DATADIR="$HOME/.local/share/docker/hub/files"
+INSTDIR="$HOME/.local/share/dockermgr/docker/hub"
+REPO="${DOCKERMGRREPO:-https://github.com/dockermgr}/hub"
 REPORAW="$REPO/raw/$REPO_BRANCH"
 APPVERSION="$(__appversion "$REPORAW/version.txt")"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup plugins
+HUB_URL=""
+SERVER_HOST=""
+SERVER_PORT=""
+SERVER_PORT_INT=""
+SERVER_PORT_SSL=""
+SERVER_PORT_SSL_INT=""
+SERVER_TIMEZONE=""
+SERVER_SSL=""
+SERVER_SSL_CRT=""
+SERVER_SSL_KEY=""
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Require a version higher than
+dockermgr_req_version "$APPVERSION"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Call the dockermgr function
+dockermgr_install
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Script options IE: --help
+show_optvars "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Requires root - no point in continuing
+#sudoreq "$0 $*" # sudo required
+#sudorun # sudo optional
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Do not update - add --force to overwrite
+#installer_noupdate "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# initialize the installer
+dockermgr_run_init
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Ensure directories exist
+ensure_dirs
+ensure_perms
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Clone/update the repo
+if am_i_online; then
+  if [ -d "$INSTDIR/.git" ]; then
+    message="Updating $APPNAME configurations"
+    execute "git_update $INSTDIR" "$message"
+  else
+    message="Installing $APPNAME configurations"
+    execute "git_clone $REPO $INSTDIR" "$message"
+  fi
+  # exit on fail
+  failexitcode $? "$message has failed"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Copy over data files - keep the same stucture as -v dataDir/mnt:/mount
+[[ -d "$INSTDIR/dataDir" ]] && cp -Rf "$INSTDIR/dataDir/*" "$DATADIR/"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Main progam
 printf_blue "Initializing the hub installer"
 # install portainer
-sudo dockermgr install portainer
+dockermgr install portainer
 # install registry
-sudo dockermgr install registry
+dockermgr install registry
 # install registry frontend
-sudo dockermgr install registry-web
+dockermgr install registry-web
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# End script
-exit $?
+# run post install scripts
+run_postinst() {
+  dockermgr_run_post
+}
+#
+execute "run_postinst" "Running post install scripts"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# create version file
+dockermgr_install_version
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# run exit function
+run_exit
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End application
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# lets exit with code
+exit ${exitCode:-$?}
