9b79582f9cb924246eb9f12ba96623e89b29f1fd
Jason <git-admin@casjaysdev.com>
2022-02-01 16:58:25 -0500

    🗃️ modified: install.sh 🗃️

--- install.sh
+++ install.sh
@@ -74,11 +74,9 @@ SERVER_PORT_INT=""
 SERVER_PORT_SSL=""
 SERVER_PORT_SSL_INT=""
 SERVER_TIMEZONE=""
+SERVER_SSLDIR=""
 SERVER_SSL_CRT=""
 SERVER_SSL_KEY=""
-[[ -f "$SERVER_SSL_CRT" ]] && [[ -f "$SERVER_SSL_KEY" ]] && SERVER_SSL="true"
-[[ -n "$SERVER_SSL" ]] || SERVER_SSL="${SERVER_SSL:-false}"
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Require a version higher than
 dockermgr_req_version "$APPVERSION"
